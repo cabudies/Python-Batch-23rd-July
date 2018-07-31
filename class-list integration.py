7a6cdc5bfdeb9367f7436258c071440c824d6033
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-31 13:08:48 +0530

    Create class-list integration.py

---
+++ class-list integration.py
@@ -0,0 +1,47 @@
+class SampleData:
+    name = ""
+    age = 0
+    phone = 0
+
+    def __init__(self, name, age, phone):
+        self.name = name
+        self.age = age
+        self.phone = phone
+
+    def displayData(self):
+        print("Name is: ", self.name)
+        print("Age is: ", self.age)
+        print("Phone is: ", self.phone)
+
+objList = []
+
+numberOfEntries = int(input("Enter the number of entries you want to make: "))
+
+for i in range(0, numberOfEntries):
+    print("Enter the details of student.")
+    name = input("Enter the name of student: ")
+    age = int(input("Enter the age of student: "))
+    phone = int(input("Enter the phone number of student: "))
+
+    obj = SampleData(name, age, phone)
+    objList.append(obj)
+
+
+
+# obj = SampleData("Ankit", 21, 123)
+# obj1 = SampleData("Ranu", 21, 123)
+# obj2 = SampleData("Vijay", 21, 123)
+#
+# objList.append(obj)
+# objList.append(obj1)
+# objList.append(obj2)
+
+# obj.displayData()
+# print("Name is: ", obj.name)
+# print("Age is: ", obj.age)
+# print("Phone is: ", obj.phone)
+
+for item in objList:
+    print("Name is: ", item.name)
+    print("Age is: ", item.age)
+    print("Phone is: ", item.phone)
