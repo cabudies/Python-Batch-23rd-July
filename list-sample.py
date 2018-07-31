d76a8d482a4f24d96b628069336143e7cdcfdbdc
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-31 12:18:58 +0530

    Add files via upload

---
+++ list-sample.py
@@ -0,0 +1,16 @@
+list = [1, 2, 3, "Ankit", 66.2, 99, "Vijay"]
+
+# print(list)
+list.append(5)
+
+list.insert(2, 65)
+
+size = len(list)
+
+print(list)
+# print(size)
+
+# print(list[2])
+print(list[2:])
+print(list[:3])
+print(list[1:4])
