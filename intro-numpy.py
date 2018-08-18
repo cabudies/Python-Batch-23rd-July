2a25b15514da7018a0c26b7f29fe295dec06aae0
Gurjas Singh <gurjassingh92@gmail.com>
2018-08-18 11:41:28 +0530

    Introduction to Numpy Array and using some functions.

---
+++ intro-numpy.py
@@ -0,0 +1,36 @@
+import numpy as np
+# from numpy import array
+
+height = [1.73, 2.86, 1.5, 1.94]
+weight = [65.4, 70.8, 62.9, 73.8]
+
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+# print(height)
+# print(weight)
+
+# print(np_height)
+# print(np_weight)
+
+# np_height = np_height + 1
+#
+# print(np_height)
+
+# bmiList = weight/height
+# print(bmiList)
+
+# bmi = np_weight/np_height ** 2
+#
+# print(bmi)
+
+# weight = weight + weight
+# print(weight)
+
+# num_array = np.array([1, 2, 3])
+#
+# num_array = num_array + num_array
+#
+# print(num_array)
+
+# Question: Why do we use array in numpy over list in python
